fa7b910886a924b38e6783b72c916cbab5a1435f
Parikhit <parikhitritzz@gmail.com>
2023-12-27 21:09:48 +0530

    core TS types

--- first-example/using-ts.ts
+++ first-example/using-ts.ts
@@ -4,6 +4,7 @@ const input2 = document.getElementById('num2')! as HTMLInputElement;
 
 const add = (num1: number, num2: number) => num1 + num2;
 
-button.addEventListener('click', () => console.log(add(+input1.value, + input2.value)));
+button.addEventListener('click', () => console.log(add(+input1.value, +input2.value)));
 
 // ! means the element is not a null
+
---
+++ understanding-typescript/ts_object.ts
@@ -0,0 +1,16 @@
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Parikhit',
+//     age: 26,
+// };
+
+//Better syntax
+
+const person = {
+    name: 'Parikhit',
+         age: 26,
+};
+
+console.log(person);
