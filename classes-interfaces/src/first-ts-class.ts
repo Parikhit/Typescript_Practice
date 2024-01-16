191df12250cd04de07a913021e53183ce9a5d55d
Parikhit <parikhitritzz@gmail.com>
2024-01-16 20:16:20 +0530

    Classes in TS

--- classes-interfaces/src/first-ts-class.ts
+++ classes-interfaces/src/first-ts-class.ts
@@ -1,11 +1,61 @@
 class Department {
-    name: string;
+    protected employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n;
+    constructor(private readonly id: string, public name: string) {}
+
+    describe(this: Department) {
+        console.log(`Department (${this.id}): ` + this.name);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
-const accounting = new Department('Accounting');
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, 'IT');
+    }
+}
 
-console.log(accounting);
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+    }
+
+    addEmployee(name: string) {
+        if (name === 'Max') return;
+
+        this.employees.push(name);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    getReports() {
+        console.log(this.reports);
+    }
+}
+
+const itDept = new ITDepartment('B1', ['Maxyy']);
+
+itDept.addEmployee('Max');
+itDept.addEmployee('Sara');
+itDept.printEmployeeInformation();
+itDept.describe();
+console.log(itDept);
+
+const accounting = new AccountingDepartment('C1', ['Billy']);
+
+accounting.addReport('Sally');
+accounting.addReport('Curran');
+accounting.getReports();
+accounting.addEmployee('Maxyyyy');
+
+accounting.printEmployeeInformation();
